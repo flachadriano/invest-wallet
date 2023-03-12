9bd5be537eaf3fa02f4188c2239619c385c09dcc
Adriano Flach de Araujo <adriano.spintec@oncologiador.corp>
2023-03-11 22:14:35 -0300

    feat(architecture): change database [#1]

--- api/src/middlewares/DataSource.ts
+++ api/src/middlewares/DataSource.ts
@@ -12,9 +12,10 @@ const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  entities: [`${__dirname}/**/entities/*.{ts,js}`],
-  migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
-  namingStrategy: new SnakeNamingStrategy()
+  entities: ['src/entities/*.{ts,js}'],
+  migrations: ['src/migrations/*.{ts,js}'],
+  namingStrategy: new SnakeNamingStrategy(),
+  ssl: true
 });
 
 export default AppDataSource;
