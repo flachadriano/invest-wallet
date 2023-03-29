edf0f02871fc9c76cc35eac4308e192d72282b89
Adriano Flach de Araujo <adriano.spintec@oncologiador.corp>
2023-03-29 12:47:05 -0300

    chore(architecture): add asset scope [#7]

--- cz/scopes.js
+++ cz/scopes.js
@@ -3,6 +3,10 @@ module.exports = {
         "description": "Architecture",
         "title": "Architecture"
     },
+    "asset": {
+        "description": "Asset",
+        "title": "Asset"
+    },
     "broker": {
         "description": "Broker",
         "title": "Broker"
